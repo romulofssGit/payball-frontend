bc2d2820987939ffc25b7744e3fb6c0b3da2c7cb
Rômulo Fernando <fssromulo@gmail.com>
2020-02-03 00:44:41 -0300

    Call endpoints from heroku

--- src/services/axios-configuration.js
+++ src/services/axios-configuration.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({baseURL: 'http://localhost:3001'});
+// const api = axios.create({baseURL: 'http://localhost:3001'});
+const api = axios.create({baseURL: 'https://payball-backend.herokuapp.com/'});
 
 export default api;
